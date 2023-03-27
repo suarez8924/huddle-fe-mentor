5aa4674d666d445b048753490f3dc3784b5056c2
suarez8924 <34613811+suarez8924@users.noreply.github.com>
2023-03-27 20:34:12 +0100

    Add CtaContent to App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,23 @@
+import { Fragment } from 'react';
+import { CtaContent } from '../CtaContent/CtaContent';
 import { Header } from '../Header/Header';
+import ScreenMockups from '../../assets/images/svg/screen-mockups.svg';
 import './App.scss';
 
 const App = () => {
   return (
-    <main className="App">
+    <Fragment>
       <Header />
-    </main>
+      <main className="app">
+        <CtaContent
+          className="main-section"
+          heading="Build The Community Your Fans Will Love"
+          body="Huddle re-imagines the way we build communities. You are a voice, but so does your audience. Create connections with your users as you engage in genuine discussion."
+          button={{ label: 'Get Started For Free', onClick: () => {} }}
+          image={ScreenMockups}
+        />
+      </main>
+    </Fragment>
   );
 };
 
