10a517f802079bb9c86e1407dedd16cfc88fc1e5
suarez8924 <34613811+suarez8924@users.noreply.github.com>
2023-03-27 20:32:50 +0100

    Add onClick property to PrimaryButton

--- src/components/PrimaryButton/PrimaryButton.tsx
+++ src/components/PrimaryButton/PrimaryButton.tsx
@@ -1,16 +1,22 @@
-import React, { PropsWithChildren } from "react";
-import "./PrimaryButton.scss";
+import React, { PropsWithChildren } from 'react';
+import './PrimaryButton.scss';
 
-type PRIMARY_BUTTON_VARIANT = "LARGE" | "REGULAR" | "SMALL";
+type PRIMARY_BUTTON_VARIANT = 'LARGE' | 'REGULAR' | 'SMALL';
 
 interface PrimaryButtonProps {
   variant?: PRIMARY_BUTTON_VARIANT;
   isFilled?: boolean;
+  onClick: () => void;
 }
 export const PrimaryButton = ({
-  variant = "REGULAR",
+  variant = 'REGULAR',
   isFilled = true,
   children,
+  onClick,
 }: PrimaryButtonProps & PropsWithChildren) => {
-  return <button className="primary-button">{children}</button>;
+  return (
+    <button className="primary-button" onClick={onClick}>
+      {children}
+    </button>
+  );
 };
